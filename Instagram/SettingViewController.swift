c47e47ee6d1621a6c80cf069a4c7e9845218be19
kaisei <kainana0015@gmail.com>
2020-07-29 08:57:06 +0900

    pod install と.gitignoreの作成まで実施

---
+++ Instagram/SettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingViewController.swift
+//  Instagram
+//
+//  Created by 岩田海靖 on 2020/07/27.
+//  Copyright © 2020 kaisei.iwata. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
